a025121591fb1ad1118daeccdeafb0dc5c220bfb
ttangelov18 <59692613+TeodorAngelov@users.noreply.github.com>
2021-07-12 10:14:00 +0300

    Add files via upload

---
+++ C++ code/Headers/global.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>
+using namespace std;
+
+struct USER_INFO
+{
+	size_t id = 0;
+	string username = "";
+	string password = "";
+	string first_name = "";
+	string last_name = "";
+	string address = "";
+	string studentName = "";
+	size_t studentAge = 0;
+	bool adminPriveleges = 0;
+};
